6d2578e1d5d5bd14593f5ef5aa115021427a1da1
arti-max <128210100+arti-max@users.noreply.github.com>
2025-03-07 00:15:00 +0300

    Add files via upload

--- stdlib/disk.asm
+++ stdlib/disk.asm
@@ -1,78 +1,39 @@
-; disk_string_tok - Progress the string pointer{disk} until character
-; Arguments:
-; si - string address
-; bx - end character
-; (also affects dynptr)
-disk_string_tok:
-.logic:
-    ldds
-    cmp %ax $F7 ; End of the disk
-    jme .error
-    cmp %ax %bx
-    jme .done
-    inx %si
-    jmp .logic
-.error:
-    mov %bx $01
-    jmp .end
-.done:
-    ldb $00
-    jme .end
-.end:
+num16 DISK_LIB_BUFFER : ?512
+
+; 1 sector - 512 bytes
+
+; Read 1 sector from disk
+; ax - sector
+; bx - save_addr
+read_sector:
+    push %bp
+    mov %bp %sp
+    ; arg 1 [bp+4]
+    mov %ax %bp
+    add %ax 4
+    mov %sp %ax
+    pop %ax
+    ; arg2 [bp+6]
+    mov %bx %bp
+    add %bx 6
+    mov %sp %bx
+    pop %bx
+    
     mov %sp %bp
-    pop %bp
-    ret
-
-; disk_string_subset - Find a specific string in the disk
-; Arguments:
-; si - address{disk}
-; gi - address to the string{mem}
-; At the end, si should point to the end of that string on the disk
-disk_string_subset:
-    mov %bx *%gi
-
-    call disk_string_tok ; Load si with the address to the first character (stored in ax)
-        cmp %bx $01
-        jme .fnf
+    ;logic
     push %gi
-    call disk_string_compare ; Compare and store the status into ax
-        cmp %ax $00 ; We found the substring (address in si)
+    mov %gi 512
+    mul %ax 512
+.logic:
+    cmp %gi $00
     jme .end
-    pop %gi
-    dex %si
+    lodb %ax %cx
+    mov %si %bx
+    storb %cx
+    inx %ax
+    inx %bx
+    dex %gi
+    jmp .logic
 
-    jmp disk_string_subset
-.end: ; File found
-    pop %x
-    mov %bx $00 ; Success status for an outer function
-    ret
-.fnf: ; File not found
-    mov %si fnf_msg
-    call puts
-    mov %bx $01 ; Error status for an outer function
-    ret
+.end:
 
-; dstrcmp - Check if two strings are equal (first pointer being on the disk)
-; Arguments:
-; si - first string address{disk}
-; gi - second string address
-; Returns:
-; ax - status
-disk_string_compare:
-  ldds
-  mov %bx *%gi
-
-  inx %si
-  inx %gi
-
-  cmp %ax %bx
-  jmne .fail
-  cmp %bx $00
-  jme .eq
-  jmp dstrcmp
-.eq:
-  mov %ax $00
-  ret
-.fail:
-  mov %ax $01
-  ret

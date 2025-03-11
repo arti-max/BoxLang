a25d50ffd1793ae1311d639643015cd100223738
arti-max <128210100+arti-max@users.noreply.github.com>
2025-03-11 12:49:54 +0300

    Update disk.asm
    Comments

--- stdlib/disk.asm
+++ stdlib/disk.asm
@@ -21,19 +21,19 @@ read_sector:
     
     mov %sp %bp
     ;logic
-    mov %cx %ax
-    mov %gi 512
-    mul %cx 512
+    mov %cx %ax ; cx = ax = sector
+    mov %gi 512 ; gi = 512 = counter (512 cycles)
+    mul %cx 512 ; cx * 512 = start byte to read & write
 .logic:
-    cmp %gi $00
+    cmp %gi $00 ; if gi == 0 then end
     jme .end
-    mov %si %cx
+    mov %si %cx ; si = cx = byte from disk to load
     ldds
-    mov %si %bx
-    storb %ax
-    inx %cx
-    inx %bx
-    dex %gi
+    mov %si %bx ; si = bx = save_address
+    storb %ax   ; save data from disk to ram
+    inx %cx     ; increment point to read from disk
+    inx %bx     ; increment save address
+    dex %gi     ; counter --
     jmp .logic
 
 .end:
